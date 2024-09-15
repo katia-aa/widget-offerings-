e63594f3658f77ff65de378544d1cc7e09cd16f9
Katia <11549223+katia-aa@users.noreply.github.com>
2024-09-15 12:56:46 -0400

    Clean up unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 import "./index.css";
 import Homepage from "./Homepage";
 
