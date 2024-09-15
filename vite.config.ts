a868ddb2b23d8cf89289166e53e25e792093d5e0
Katia <11549223+katia-aa@users.noreply.github.com>
2024-09-15 13:29:37 -0400

    Fix CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/widget-offerings-/",
+  build: {
+    outDir: "dist", // This should point to your output directory
+    rollupOptions: {
+      input: "index.html", // Ensure the input HTML is correct
+    },
+  },
 });
